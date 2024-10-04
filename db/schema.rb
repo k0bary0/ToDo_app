46b84caac3d59aad5979a618c5b2905cbe0af5cd
k0bary0 <ryota.drums.3160@gmail.com>
2024-10-04 19:19:28 +0900

    change the migratino order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_13_162749) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_04_101649) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
