d62c8619250cfc1c2159954a82ad17196dcc8f11
k0bary0 <ryota.drums.3160@gmail.com>
2024-10-04 21:18:46 +0900

    Change the migration order (second time)

--- db/migrate/20241004121342_create_users.rb
+++ db/migrate/20241004121342_create_users.rb
@@ -9,3 +9,4 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     end
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_04_101649) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 2024_10_04_121658) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
